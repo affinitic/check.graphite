92171b57a2810730f5c0c8005fb9ee91cbcc58fb
Martin Peeters <martin.peeters@affinitic.be>
2014-12-16 09:45:11 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+version = '0.1a1'
+
+long_description = (
+    open('README.txt').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(
+    name='check.graphite',
+    version=version,
+    description="Script to pull graphite data with nagios",
+    long_description=long_description,
+    # Get more strings from
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Programming Language :: Python',
+    ],
+    keywords='',
+    author='',
+    author_email='',
+    url='http://github.com/affinitic/check.graphite/',
+    license='gpl',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['check'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+)
