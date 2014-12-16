c7d48e2e2e8cfb5bcd89bcee82ed76826d9b5fdf
Martin Peeters <martin.peeters@affinitic.be>
2014-12-16 16:51:17 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,32 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+# coding=utf-8
 import os
+import sys
+
+
+def running_under_virtualenv():
+    if hasattr(sys, 'real_prefix'):
+        return True
+    elif sys.prefix != getattr(sys, "base_prefix", sys.prefix):
+        return True
+    if os.getenv('VIRTUAL_ENV', False):
+        return True
+    return False
+
+
+if os.environ.get('USE_SETUPTOOLS'):
+    from setuptools import setup
+    setup  # workaround for pyflakes issue #13
+    setup_kwargs = dict(zip_safe=0)
+else:
+    from distutils.core import setup
+    setup_kwargs = dict()
 
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'version.txt')) as f:
     version = f.read().strip()
+
+data_files = [('/usr/bin', ['script/check_graphite'])]
 
 long_description = (
     open('README.rst').read()
@@ -31,17 +54,18 @@ setup(
     author_email='',
     url='http://github.com/affinitic/check.graphite/',
     license='gpl',
-    packages=find_packages('src'),
+    packages=['check', 'check.graphite'],
     package_dir={'': 'src'},
     namespace_packages=['check'],
     include_package_data=True,
     zip_safe=False,
+    data_files=data_files,
     install_requires=[
         'setuptools',
         'requests',
     ],
     entry_points={
         'console_scripts': [
-            'check_graphite = check.graphite.script:main',
+            'check_graphite = check.graphite.script:check_graphite',
         ]},
 )
