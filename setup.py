30ac536b69af9e139b33cea5eeb5d14f23f324b4
Martin Peeters <martin.peeters@affinitic.be>
2014-12-16 14:18:29 +0100

    Add buildout files

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from setuptools import setup, find_packages
 version = '0.1a1'
 
 long_description = (
-    open('README.txt').read()
+    open('README.rst').read()
     + '\n' +
     'Contributors\n'
     '============\n'
     + '\n' +
-    open('CONTRIBUTORS.txt').read()
+    open('CONTRIBUTORS.rst').read()
     + '\n' +
-    open('CHANGES.txt').read()
+    open('CHANGES.rst').read()
     + '\n')
 
 setup(
@@ -35,6 +35,7 @@ setup(
     zip_safe=False,
     install_requires=[
         'setuptools',
+        'requests',
     ],
     entry_points="""
     # -*- Entry points: -*-
