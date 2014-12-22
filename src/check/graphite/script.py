0038943fef77e24b04bab7bcfebb38c88abb13fa
Martin Peeters <martin.peeters@affinitic.be>
2014-12-22 12:17:36 +0100

    Add the possibility to send multiple metrics

--- src/check/graphite/script.py
+++ src/check/graphite/script.py
@@ -20,8 +20,9 @@ def check_graphite():
     parser = argparse.ArgumentParser()
     parser.add_argument('host', type=str,
                         help='Graphite host e.g. http://host.com/render')
-    parser.add_argument('metric', type=str)
-    parser.add_argument('-n', '--name', type=str, default=None)
+    parser.add_argument('metrics', type=str,
+                        help='metric names separate by a comma')
+    parser.add_argument('-n', '--name', type=str, default='undefined')
     parser.add_argument('-t', '--timeframe', type=str, default='30seconds')
     parser.add_argument('-w', '--warning', type=float, default=100.0)
     parser.add_argument('-c', '--critical', type=float, default=200.0)
@@ -35,8 +36,14 @@ def check_graphite():
 class CheckGraphite(object):
 
     status_match = {
+        'unknown': 3,
         'critical': 2,
         'warning': 1,
+        'ok': 0,
+        0: 'ok',
+        1: 'warning',
+        2: 'critical',
+        3: 'unknown',
     }
 
     def __init__(self, **kw):
@@ -49,26 +56,35 @@ class CheckGraphite(object):
             self.status.reverse()
 
     def process(self):
+        results = []
+        for url in self.urls:
+            results.append(self.get_result(url))
+        status = sorted([r[0] for r in results], reverse=True)[0]
+        info = '%s - %s - %s' % (self.status_match.get(status).upper(),
+                                 self.name,
+                                 ', '.join([r[1] for r in results]))
+        return status, info
+
+    def get_result(self, url):
         try:
-            result = json.loads(requests.get(self.url).content)
+            result = json.loads(requests.get(url).content)
         except ValueError:
             result = None
         if not result:
-            return 3, 'UNKNOWN - no data'
-        if self.name is None:
-            self.name = result[0].get('target')
+            return 3, 'no data'
         return self.process_result(result[0])
 
     def process_result(self, result):
         if len(result.get('datapoints', [])) == 0:
-            return 3, 'UNKNOWN - no valid datapoints'
+            return 3, 'no valid datapoints'
         values = [d[0] for d in result.get('datapoints') if d[0]]
         avg = values and self.get_average(values) or 0
+        name = result.get('target').split('.')[-1]
         for status in self.status:
             if self.op(avg, getattr(self, status)):
                 return (self.status_match.get(status),
-                        '%s - %s=%s' % (status.upper(), self.name, avg))
-        return 0, 'OK - %s=%s' % (self.name, avg)
+                        '%s=%s' % (status.upper(), name, avg))
+        return 0, '%s=%s' % (name, avg)
 
     @staticmethod
     def handle_gzip_content(content):
@@ -77,9 +93,15 @@ class CheckGraphite(object):
         return f.read()
 
     @property
-    def url(self):
+    def urls(self):
         url_format = '%(host)s?target=%(metric)s&from=-%(timeframe)s&format=json'
-        return url_format % self.__dict__
+        metrics = self.metrics.split(',')
+        values = self.__dict__
+        urls = []
+        for metric in metrics:
+            values['metric'] = metric
+            urls.append(url_format % values)
+        return urls
 
     @staticmethod
     def get_average(values):
